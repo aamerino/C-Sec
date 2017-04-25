3ca0de1dfbc9b92dda70767f881c3aa1ca0e1db1
Aitor <amerinotsutsui@gmail.com>
2017-04-25 17:56:21 +0200

    Changing color of droppable depending of draggable

--- js/js.js
+++ js/js.js
@@ -4,6 +4,11 @@ $( function() {
 		containment: ".edifici"
 	});
 	$( ".punt-vigilancia").droppable({
+		accept: function(d) {
+			if(d.hasClass("vigilant")) {
+				return true;
+			}
+		},
 		drop: function( event, ui) {
 			$( this )
 			.addClass( "punt-vigilancia-actiu" )
@@ -11,6 +16,21 @@ $( function() {
 		out: function (event, ui) {
 			$( this)
 			.removeClass( "punt-vigilancia-actiu" )
+		}
+	});
+	$( ".punt-supervisio").droppable({
+		accept: function(d) {
+			if(d.hasClass("supervisor")) {
+				return true;
+			}
+		},
+		drop: function( event, ui) {
+			$( this )
+			.addClass( "punt-supervisio-actiu" )
+		},
+		out: function (event, ui) {
+			$( this)
+			.removeClass( "punt-supervisio-actiu" )
 		}
 	});
 } );
