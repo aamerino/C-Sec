c3befc78e930b90b7f78473e8e79541655c343cb
Ramon de la Cruz Martinez <ramondelacruzmartinez@gmail.com>
2017-04-26 18:39:20 +0200

    5. add sortable

--- js/js.js
+++ js/js.js
@@ -1,36 +1,45 @@
-$( function() {
-	$( ".vigilant").draggable();
-	$( ".supervisor").draggable({
-		containment: ".edifici"
-	});
-	$( ".punt-vigilancia").droppable({
-		accept: function(d) {
-			if(d.hasClass("vigilant")) {
-				return true;
-			}
-		},
-		drop: function( event, ui) {
-			$( this )
-			.addClass( "punt-vigilancia-actiu" )
-		},
-		out: function (event, ui) {
-			$( this)
-			.removeClass( "punt-vigilancia-actiu" )
-		}
-	});
-	$( ".punt-supervisio").droppable({
-		accept: function(d) {
-			if(d.hasClass("supervisor")) {
-				return true;
-			}
-		},
-		drop: function( event, ui) {
-			$( this )
-			.addClass( "punt-supervisio-actiu" )
-		},
-		out: function (event, ui) {
-			$( this)
-			.removeClass( "punt-supervisio-actiu" )
-		}
-	});
-} );
+$(function () {
+    $(".vigilant").draggable({
+        cursor: 'pointer'
+    });
+    $(".supervisor").draggable({
+        containment: ".edifici",
+        cursor: 'pointer'
+    });
+    $(".punt-vigilancia").droppable({
+        accept: function (d) {
+            if (d.hasClass("vigilant")) {
+                return true;
+            }
+        },
+        drop: function (event, ui) {
+            $(this)
+                .addClass("punt-vigilancia-actiu")
+        },
+        out: function (event, ui) {
+            $(this)
+                .removeClass("punt-vigilancia-actiu")
+        }
+    });
+    $(".punt-supervisio").droppable({
+        accept: function (d) {
+            if (d.hasClass("supervisor")) {
+                return true;
+            }
+        },
+        drop: function (event, ui) {
+            $(this)
+                .addClass("punt-supervisio-actiu")
+        },
+        out: function (event, ui) {
+            $(this)
+                .removeClass("punt-supervisio-actiu")
+        }
+    });
+
+    $("#llistat-productes").sortable();
+
+    $("#desplegar-llistat").click(function () {
+        $("#llistat-productes").slideToggle("fast");
+    });
+});
