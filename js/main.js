d6c8056932dabbcc004446a6356a24f074e5707c
Ramon de la Cruz Martinez <ramondelacruzmartinez@gmail.com>
2017-05-05 17:28:20 +0200

    coments js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,43 @@
 $(function () {
     comprobarEstats();
-
+/*   Als elements amb la classe "vigilant" els afegim la funcionalitat "draggable" per poder moure'ls per la pantalla
+    li afegim la propietat "cursor: 'move'" perque en moure els elements el cursor canviï per una creu */
     $(".vigilant").draggable({
         cursor: 'move'
     });
+   //Aquí fem el mateix amb el supervisor i limitem el seu moviment a l'edifici  amb la propietat "containment"
     $(".supervisor").draggable({
         containment: ".edifici",
         cursor: 'move'
     });
+    //seleccionem l'element marcat amb la classe "punt-vigilancia" per afegir-li la funcionalitat droppable de jquery
     $(".punt-vigilancia").droppable({
         accept: function (d) {
+            //vam comprovant que l'element introduït està marcat amb la classe "vigilant" per acceptar-lo
             if (d.hasClass("vigilant")) {
                 return true;
             }
         },
         drop: function (event, ui) {
             $(this)
-                .addClass("punt-vigilancia-actiu")
+                //Quan el punt de vigilància té un element l'hi afegim una classe per canviar-li el color
+                .addClass("punt-vigilancia-actiu");
+            //I comprovam els estats
             comprobarEstats();
         },
         out: function (event, ui) {
             $(this)
-                .removeClass("punt-vigilancia-actiu")
+                // //Quan el punt de vigilància NO té un element l'hi afegim una classe per canviar-li el color
+                .removeClass("punt-vigilancia-actiu");
+            ////I comprovam els estats
             comprobarEstats();
         },
+        //li afegim la propietat "tolerance" per indicar-li que només estigui actiu quan tot l'element estigui endins
         tolerance: "fit"
     });
+    //Tambe li afegim la funcionalitat droppable a l'element marcat amb la clase "punt-supervisio"
     $(".punt-supervisio").droppable({
+        //comprobam que l'element te la clase "supervisor" perque es pugui fica.
         accept: function (d) {
             if (d.hasClass("supervisor")) {
                 return true;
@@ -34,21 +45,27 @@ $(function () {
         },
         drop: function (event, ui) {
             $(this)
-                .addClass("punt-supervisio-actiu")
+            //cambiem el color de l'element una vegada te un supervisor ficat.
+                .addClass("punt-supervisio-actiu");
             comprobarEstats();
         },
         out: function (event, ui) {
             $(this)
-                .removeClass("punt-supervisio-actiu")
+            //I llevam el color quan el traguem.
+                .removeClass("punt-supervisio-actiu");
+            //comprobam els estats.
             comprobarEstats();
         },
         tolerance: "fit"
     });
 
+/*    Marquem la llista desordenada dins de l'element "departaments" amb la funcionalitat sortable  de jquery per poder modificar
+    l'ordre del llistat agafant y soltant elements, li afegim la propietat "cursor:move" perque aparegui una ma quan agafam elements del llistat */
     $("#departaments > ul").sortable({
         cursor: 'move'
     });
 
+    //Deixem d'amagar l'element marcat amb "desplegarLlistat" i despleguem l'element en fer clic.
     $(".desplegarLlistat").click(function (event) {
         $(event.target).next().slideToggle("fast");
     });
@@ -126,22 +143,26 @@ $(function () {
         }
     });
 
+    //Apliquem la funció accordion() de jquery  a l'element "accordion" per per afegir-la seva funcionalitat, li afegim
+    // la propietat "collapsible: true" perquè  es pugui tancar
     $("#accordion").accordion({
         collapsible: true
     });
-
+    //Apliquem la funció tabs() de jquery  a l'element "cameras-seguretat" per afegir-la seva funcionalitat.
     $("#cameras-seguretat").tabs({
         collapsible: true
     });
 });
 
 function comprobarEstats() {
+    //Aquesta funció comprova l'estat de les portes per saber si estan tancades o obertes
     var portaVendes = $(".portaVendes");
     var portaInterior = $(".portaInterior");
     var portaMagatzem = $(".portaMagatzem");
     var puntVigilancia = $(".punt-vigilancia");
     var puntSupervisio = $(".punt-supervisio");
 
+    //Si totes les portes estan tancades fem visible l'element html amb el missatge "Totes les portes estan tancades".
     if (portaVendes.hasClass("tancada") && portaInterior.hasClass("tancada") &&
         portaMagatzem.hasClass("tancada")) {
         $("#portesTancades").show();
@@ -149,6 +170,7 @@ function comprobarEstats() {
         $("#portesTancades").hide();
     }
 
+    //El mateix amb el punt de vigilància, alternant entre ensenyar l'element amb el missatge "Tots els punts estan atesos" o no.
     if (puntVigilancia.hasClass("punt-vigilancia-actiu") &&
         puntSupervisio.hasClass("punt-supervisio-actiu")) {
         $("#puntsAtesos").show();
